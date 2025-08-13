d102bfe52c3ff26fdab6a56cbea67803dd05615e
Giovana Vieira de Morais <giovana.vmorais@gmail.com>
2025-08-13 18:41:05 -0400

    test sh

---
+++ scripts/sh/run_qwentest.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+base_path="/scratch/gv2167/2025_investigating_mmshap"
+environment="hpc"
+system_instruction="default"
+range=1
+index=0 #$SLURM_ARRAY_TASK_ID
+model="qwenaudio"
+input_path="${base_path}/data/input_data/muchomusic_musiccaps_fs.json"
+
+python \
+	$base_path/src/experiments/qwenaudio_experiment.py \
+	--input_path=$input_path	\
+	--model=$model 			\
+	--environment=$environment 	\
+	--range=$range 			\
+	--index=$index
